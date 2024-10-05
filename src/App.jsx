092bacb1ce171ef70ff9311644e78a7a054dd280
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-10-05 12:37:23 +0600

    Board Ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+function Square({ value }) {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+      {value}
+    </button>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  let i = 0;
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Square value={i++} />
+        <Square value={i++} />
+        <Square value={i++} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <Square value={i++} />
+        <Square value={i++} />
+        <Square value={i++} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <Square value={i++} />
+        <Square value={i++} />
+        <Square value={i++} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
